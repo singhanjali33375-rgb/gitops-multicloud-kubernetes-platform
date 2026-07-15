70c29139bd85a95b54ed8692c08bac551d230dfb
Anjali Singh <singhanjali33375@gmail.com>
2026-07-15 13:54:29 +0530

    Create main.tf

---
+++ terraform/modules/vpc/terraform/modules/vpc/main.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "main" {
+
+  cidr_block = var.cidr_block
+
+  enable_dns_hostnames = true
+
+  enable_dns_support = true
+
+  tags = {
+
+    Name = "${var.project_name}-vpc"
+
+  }
+
+}
