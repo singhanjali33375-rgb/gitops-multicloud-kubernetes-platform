b4678154c49bec70328ba2b65fc992898347f38b
Anjali Singh <singhanjali33375@gmail.com>
2026-07-15 13:52:08 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,36 @@
-terraform {
-  required_version = ">= 1.7.0"
+module "vpc" {
+  source = "./modules/vpc"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  cidr_block = "10.0.0.0/16"
+
+  public_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  private_subnets = [
+    "10.0.11.0/24",
+    "10.0.12.0/24"
+  ]
+
+  availability_zones = [
+    "ap-south-1a",
+    "ap-south-1b"
+  ]
 }
 
-# Resources will be added here
+module "eks" {
+  source = "./modules/eks"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  cluster_name = "gitops-eks"
+
+  subnet_ids = module.vpc.private_subnet_ids
+
+  vpc_id = module.vpc.vpc_id
+}
