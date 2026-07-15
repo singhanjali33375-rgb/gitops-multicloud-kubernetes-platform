868cdff9bb305eb6930102b5b5f27e0b42fa1137
Anjali Singh <singhanjali33375@gmail.com>
2026-07-15 13:53:35 +0530

    Create variables.tf

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,17 @@
+variable "project_name" {}
+
+variable "environment" {}
+
+variable "cidr_block" {}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
